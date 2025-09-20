bc4e8a649d16bd380ee1558ada474917aa33c24f
Madhur Bharti <madhur@Madhurs-MacBook-Pro.local>
2025-09-20 19:44:52 +0530

    All Works ⚡️

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="bg-red-500 text-4xl">Hello world</p>
+    </>
+  );
+}
+
+export default App;
